93e547488410cf095ea20ffb5318dfac1d8c8509
araethomp <angela.r.thomp@gmail.com>
2021-01-10 14:12:36 -0800

    Set base variables to 0

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,16 +2,28 @@ import os
 import csv
 
 csvpath = os.path.join('Resources', 'budget_data.csv')
+output_file = os.path.join('analysis', 'pybank.txt')
+
+total_months = 0
+net_total = 0
+previous_budget = 0
+greatest_increase = 0
+greatest_decrease = 0
+greatest_increase_date = ""
+greatest_decrease_date = ""
+
 
 with open(csvpath, newline='') as csvfile:
 
     csvreader = csv.reader(csvfile, delimiter=',')
 
-    print(csvreader)
+    #print(csvreader)
 
     csv_header = next(csvreader)
     print(f"CSV Header: {csv_header}")
 
     for row in csvreader:
-        print(row)
-        
+       print(row)
+
+
+
