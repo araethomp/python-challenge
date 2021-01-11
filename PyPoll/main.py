e189fc590f37503fcca7fb81ed6f0a82794ec0b2
araethomp <angela.r.thomp@gmail.com>
2021-01-10 20:14:55 -0800

    Added total votes, added potential variables to 0

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -4,6 +4,13 @@ import csv
 csvpath = os.path.join('Resources', 'election_data.csv')
 output_file = os.path.join('analysis', 'pypoll.txt')
 
+total_votes = 0
+Khan_votes = 0
+Correy_votes = 0
+Li_votes = 0
+OTooley_votes = 0
+winner = ""
+candidates = ['Khan', 'Correy', 'Li', 'OTooley']
 
 with open(csvpath, newline='') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
@@ -11,11 +18,18 @@ with open(csvpath, newline='') as csvfile:
     csv_header = next(csvreader)
     print(f"CSV Header: {csv_header}")
 
+    for row in csvreader:
+        total_votes += 1
+
+      
+
 
 
 
 output = f"""
 Election Results
+---------------------
+Total Votes: {total_votes}
 ---------------------
 Khan: %, (total votes)
 Correy: %, (total votes)
