a1a76d9faccffbec0d0c5521cc8c9c783b57d70f
araethomp <angela.r.thomp@gmail.com>
2021-01-10 19:48:55 -0800

    Imported csv file, created output multi-line string

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,1 +1,28 @@
+import os
+import csv
 
+csvpath = os.path.join('Resources', 'election_data.csv')
+output_file = os.path.join('analysis', 'pypoll.txt')
+
+
+with open(csvpath, newline='') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    csv_header = next(csvreader)
+    print(f"CSV Header: {csv_header}")
+
+
+
+
+output = f"""
+Election Results
+---------------------
+Khan: %, (total votes)
+Correy: %, (total votes)
+Li: %, (total votes)
+O'Tooley: %, (total votes)
+---------------------
+Winner:
+---------------------
+"""
+print(output)
